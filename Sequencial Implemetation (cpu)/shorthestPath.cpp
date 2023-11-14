6b155f811c369e6ceff5289c4c1ee50dfc345b5d
pdrzanFilho <contatesteparalogins@gmail.com>
2023-11-14 18:35:25 -0400

    feature: sequencial implementation (cpu) improved

--- Sequencial Implemetation (cpu)/shorthestPath.cpp
+++ Sequencial Implemetation (cpu)/shorthestPath.cpp
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <math.h>
 #include <iostream>
+#include <typeinfo>
 #include "../Lib/cpu.h"
 #include "../Lib/init.h"
+#include "../Lib/print_matrix.h"
 
 int main(int argc, char *argv[])
 {
@@ -12,60 +15,22 @@ int main(int argc, char *argv[])
     float *matrixA, *matrixB;
     int n = atoi(argv[1]);
 
+    bool printMatrixOption;
+
+    if(strcmp(argv[2], "yes") != 0)
+    {
+        printMatrixOption = false;
+    }
+    else
+    {
+        printMatrixOption = true;
+    }
+
     matrixA = (float *)malloc(sizeof(float) * n * n);
     matrixB = (float *)malloc(sizeof(float) * n * n);
-    for (int i = 0; i < n * n; i++)
-    {
-        matrixA[i] = __FLT_MAX__;
-    }
-    // 0 2 3 ∞ ∞ ∞ ∞ ∞ ∞
-    // ∞ 0 ∞ ∞ ∞ 1 ∞ ∞ ∞
-    // ∞ ∞ 0 1 2 ∞ ∞ ∞ ∞
-    // ∞ ∞ ∞ 0 ∞ ∞ 2 ∞ ∞
-    // ∞ ∞ ∞ ∞ 0 ∞ ∞ ∞ ∞
-    // ∞ ∞ ∞ ∞ ∞ 0 2 3 2
-    // ∞ ∞ ∞ ∞ 1 ∞ 0 1 ∞
-    // ∞ ∞ ∞ ∞ ∞ ∞ ∞ 0 ∞
-    // ∞ ∞ ∞ ∞ ∞ ∞ ∞ 1 0
-    matrixA[0] = 0;
-    matrixA[1] = 2;
-    matrixA[2] = 3;
 
-    matrixA[9 * 1 + 1] = 0;
-    matrixA[9 * 1 + 5] = 1;
-
-    matrixA[9 * 2 + 2] = 0;
-    matrixA[9 * 2 + 3] = 1;
-    matrixA[9 * 2 + 4] = 2;
-
-    matrixA[9 * 3 + 3] = 0;
-    matrixA[9 * 3 + 6] = 2;
-
-    matrixA[9 * 4 + 4] = 0;
-
-    matrixA[9 * 5 + 5] = 0;
-    matrixA[9 * 5 + 6] = 2;
-    matrixA[9 * 5 + 7] = 3;
-    matrixA[9 * 5 + 8] = 2;
-
-    matrixA[9 * 6 + 4] = 1;
-    matrixA[9 * 6 + 6] = 0;
-    matrixA[9 * 6 + 7] = 1;
-
-    matrixA[9 * 7 + 7] = 0;
-
-    matrixA[9 * 8 + 7] = 1;
-    matrixA[9 * 8 + 8] = 0;
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = 0; j < n; j++)
-        {
-            std::cout << matrixA[i*n + j] << ' ';
-        }
-        std::cout << '\n';
-    }
-    // initDataRandom(matrixA, n * n);
-
+    initDataRandom(matrixA, n * n);
+    
     bool isA = true;
     for (int i = 1; i < n * 2; i = i << 1)
     {
@@ -81,26 +46,15 @@ int main(int argc, char *argv[])
         }
     }
 
-    if (isA)
+    if(printMatrixOption)
     {
-        for(int i = 0; i < n; i++)
+        if (isA)
         {
-            for(int j = 0; j < n; j++)
-            {
-                std::cout << matrixB[i*n + j] << ' ';
-            }
-            std::cout << '\n';
+            printMatrix(matrixB, n * n);
         }
-    }
-    else
-    {
-        for(int i = 0; i < n; i++)
+        else
         {
-            for(int j = 0; j < n; j++)
-            {
-                std::cout << matrixA[i*n + j] << ' ';
-            }
-            std::cout << '\n';
+            printMatrix(matrixA, n * n);
         }
     }
 
