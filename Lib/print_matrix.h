a46c8e5140e43d99674bff004182845fc97718d2
pdrzan <pedeozanelato19@gmail.com>
2023-11-14 21:11:55 -0400

    feature: Print format

--- Lib/print_matrix.h
+++ Lib/print_matrix.h
@@ -1,11 +1,18 @@
-void printMatrix(float *matrix, int size)
+void print_matrix(float *matrix, int size)
 {
     int n = sqrt(size);
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            std::cout << matrix[i * n + j] << ' ';
+            if(matrix[i * n + j] == __FLT_MAX__)
+            {
+                std::cout << std::setw(5) << "inf" << ' ';
+            }
+            else
+            {
+                std::cout << std::setw(5) << matrix[i * n + j] << ' ';
+            }
         }
         std::cout << '\n';
     }
