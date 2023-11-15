fb40e7aac4e76c9550957224996a3c0d28180c28
pdrzan <pedeozanelato19@gmail.com>
2023-11-14 22:01:56 -0400

    feature: Initial First Implementation

--- First Implementation/shorthest_path.cu
+++ First Implementation/shorthest_path.cu
@@ -1,12 +1,14 @@
-#define N 1024 // Matrix NxN
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <math.h>
+#include <iostream>
+#include <iomanip> 
 #include "../Lib/init.h"
 #include "../Lib/verify.h"
 #include "../Lib/cpu.h"
+#include "../Lib/time_analysis.h"
 #include "matrix_tiled.h"
 
 int main(int argc, char *argv[])
@@ -14,27 +16,72 @@ int main(int argc, char *argv[])
 
     int device_id, number_of_sms;
     clock_t start, end;
+    int n = atoi(argv[1]);
+    bool print_matrix_option;
+
+    if(argv[2] == nullptr || strcmp(argv[2], "yes") != 0)
+    {
+        print_matrix_option = false;
+    }
+    else
+    {
+        print_matrix_option = true;
+    }
 
     cudaDeviceProp device_prop;
     cudaGetDeviceProperties(&device_prop, 0);
-    printf("device %d: %s \n", 0, device_prop.name);
+    printf("Device %D: %s \n", 0, device_prop.name);
     cudaSetDevice(0);
 
-    size_t bytes = N * N * sizeof(float);
+    size_t bytes = n * n * sizeof(float);
 
-    printf("Matrix memory occupation %d\n", bytes);
+    std::cout << "Matrix memory occupation" << bytes << '\n';
 
-    float *h_a, *h_b, *h_cpu, *h_naive, *h_tiled;
-    float *d_a, *d_b, *d_naive, *d_tiled;
+    float *h_a, *h_b, *h_cpu, *h_tiled;
+    float *d_a, *d_b, *d_tiled;
     float time_cpu, time_gpu_naive, time_gpu_tiled;
 
     h_a = (float *)malloc(bytes);
     h_b = (float *)malloc(bytes);
-    h_cpu = (float *)malloc(bytes);
+    // h_cpu = (float *)malloc(bytes);
     h_tiled = (float *)malloc(bytes);
 
-    init_data_random(h_a, N * N);
-    init_data_random(h_b, N * N);
+    init_data_random(h_a, n * n);
+    init_data_random(h_b, n * n);
+    // memset(h_cpu, 0, bytes);
+    memset(h_tiled, 0, bytes);
+
+    cudaMalloc(&d_a, bytes);
+    cudaMalloc(&d_b, bytes);
+    cudaMalloc(&d_tiled, bytes);
+
+    cudaMemcpy(d_a, h_a, bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b, bytes, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_tiled, h_tiled, bytes, cudaMemcpyHostToDevice);
+
+    // start = clock();
+    // cpu_mmatrix(h_cpu, h_a, h_b, n);
+    // end = clock();
+
+    int n_threads = 32;
+    int n_blocks = n / threads;
+
+    dim3 threads(n_threads, n_threads);
+    dim3 blocks(n_blocks, n_blocks);
+
+    std::cout << "Blocks: " << n_blocks << '\n';
+    std::cout << "Threads/block: " << n_threads << '\n';
+    std::cout << "Threads(total) " << n_threads * n_blocks << '\n';
+
+    time_start();
+    matrixMul_tiled<<<blocks, threads>>>(d_tiled, d_a, d_b, n);
+    cudaDeviceSynchronize();
+    time_end();
+    cudaMemcpy(h_tiled, d_tiled, bytes, cudaMemcpyDeviceToHost);
+    std::cout << "Time: " << elapsed_time << '\n';
+
+    free(h_cpu); free(h_tiled); free(h_a); free(h_b); 
+    cudaFree(d_tiled); cudaFree(d_a); cudaFree(d_b);
 
     return 0;
 }
