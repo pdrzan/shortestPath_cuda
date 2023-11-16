69f889b0be0220ce3c84a21078f8cebd6aff2379
pdrzanFilho <contatesteparalogins@gmail.com>
2023-11-16 14:15:06 +0000

    feature: Matrix tiled implemented

--- First Implementation/matrix_tiled.h
+++ First Implementation/matrix_tiled.h
@@ -1,38 +1,39 @@
-const int SHMEM_SIZE = N;
-
-__global__ void matrixMul_tiled(float *c, float *a, float *b, int n)
+__global__ void matrixMul_tiled(float *a, int n)
 {
     // Compute each thread's global row and column index
     int row = blockIdx.y * blockDim.y + threadIdx.y;
     int col = blockIdx.x * blockDim.x + threadIdx.x;
 
     // Statically allocated shared memory
-    __shared__ int s_a[SHMEM_SIZE];
-    __shared__ int s_b[SHMEM_SIZE];
+    __shared__ int s_a[n];
+    __shared__ int s_b[n];
 
     // Accumulate in temporary variable
-    int tmp = 0;
+    int tmp = __FLT_MAX__;
 
     // Sweep tile across matrix
-    for (int i = 0; i < n; i += blockDim.x)
+    for (int k = 1; k < n * 2; k = k << 1)
     {
-        // Load in elements for this tile
-        s_a[threadIdx.y * blockDim.x + threadIdx.x] = a[row * n + i + threadIdx.x];
-        s_b[threadIdx.y * blockDim.x + threadIdx.x] = b[i * n + threadIdx.y * n + col];
-
-        // Wait for both tiles to be loaded in before doing computation
-        __syncthreads();
-
-        // Do matrix multiplication on the small matrix
-        for (int j = 0; j < blockDim.x; j++)
+        for (int i = 0; i < n; i += blockDim.x)
         {
-            tmp += s_a[threadIdx.y * blockDim.x + j] * s_b[j * blockDim.x + threadIdx.x];
+            // Load in elements for this tile
+            s_a[threadIdx.y * blockDim.x + threadIdx.x] = a[row * n + i + threadIdx.x];
+            s_b[threadIdx.y * blockDim.x + threadIdx.x] = a[i * n + threadIdx.y * n + col];
+
+            // Wait for both tiles to be loaded in before doing computation
+            __syncthreads();
+
+            // Do matrix multiplication on the small matrix
+            for (int j = 0; j < blockDim.x; j++)
+            {
+                tmp = std::min(s_a[threadIdx.y * blockDim.x + j] + s_b[j * blockDim.x + threadIdx.x], tmp);
+            }
+
+            // Wait for all threads to finish using current tiles before loading in new ones
+            __syncthreads();
         }
 
-        // Wait for all threads to finish using current tiles before loading in new ones
-        __syncthreads();
+        // Write back results
+        a[row * n + col] = tmp;
     }
-
-    // Write back results
-    c[row * n + col] = tmp;
 }
